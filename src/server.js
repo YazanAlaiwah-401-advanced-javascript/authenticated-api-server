4a8f3d580bbbd83f008235e486b8aae9d5140072
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-06-14 19:23:22 +0300

    test

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 'use strict';
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const notFound = require('./middleware/errors/404.js');
 const errorHandler = require('./middleware/errors/500.js');
 // const Auth = require('./routes/auth.js');
@@ -9,7 +9,7 @@ const Admin = require('./routes/admin.routes.js');
 
 const app = express();
 app.use(express.json());
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(express.static('public'));
 
 // app.use('/api/v1/auth',Auth);
